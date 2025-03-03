eb62fce66f6f4bb019a147b1dee8b73cc490535a
sondosomarr <sondosomarr2020@gmail.com>
2025-03-03 07:37:54 +0200

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  
+
+  return (
+    <div className='text-red-500 text-4xl bg-blue-500'>
+      hello world
+      it's a great thing
+    </div>
+  )
+}
+
+export default App
