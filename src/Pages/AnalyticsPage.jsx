d3c3d81957d03fee2f15c3cdda941bb443081634
sondosomarr <sondosomarr2020@gmail.com>
2025-03-04 06:32:31 +0200

    feat: add new pages for users, sales, orders, analytics, and settings with headers

---
+++ src/Pages/AnalyticsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../Components/common/Header'
+
+const AnalyticsPage = () => {
+  return (
+     <div className='flex-1 overflow-auto relative z-10'>
+          <Header title=" Analytics">
+    
+          </Header>
+        </div>
+  )
+}
+
+export default AnalyticsPage
---
+++ src/Pages/SettingsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../Components/common/Header'
+
+const SettingsPage = () => {
+  return (
+     <div className='flex-1 overflow-auto relative z-10'>
+          <Header title="Settings">
+    
+          </Header>
+        </div>
+  )
+}
+
+export default SettingsPage
