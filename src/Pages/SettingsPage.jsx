d47379cd07427e0964fa9b60a31056f417985c11
sara khaled <sarahkhaled22003@gmail.com>
2025-03-08 08:38:28 +0200

    feat: add products-orders- settings components

--- src/Pages/SettingsPage.jsx
+++ src/Pages/SettingsPage.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Header } from '../Components/common/Header'
+import {Header} from "../Components/common/Header";
+import ConnectedAccounts from "../Components/settings/ConnectedAccounts";
+import DangerZone from "../Components/settings/DangerZone";
+import Notifications from "../Components/settings/Notifications";
+import Profile from "../Components/settings/profile";
+import Security from "../Components/settings/Security";
 
 const SettingsPage = () => {
-  return (
-     <div className='flex-1 overflow-auto relative z-10'>
-          <Header title="Settings">
-    
-          </Header>
-        </div>
-  )
-}
-
-export default SettingsPage
+	return (
+		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+			<Header title='Settings' />
+			<main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
+				<Profile />
+				<Notifications />
+				<Security />
+				<ConnectedAccounts />
+				<DangerZone />
+			</main>
+		</div>
+	);
+};
+export default SettingsPage;
